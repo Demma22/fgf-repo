d93bad2603f323cf428f77544c3b3405d74e3298
paulie <pktpaulie@gmail.com>
2023-09-16 19:18:09 +0300

    Add Dockerfile

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .import models
 # Register your models here.
+
+
+
+admin.site.register(models.Contributer)
+admin.site.register(models.Admin)
+admin.site.register(models.User_Contribution)
